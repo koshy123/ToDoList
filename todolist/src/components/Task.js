4158102c08b277fc1a6f713703014627981f81c3
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-11-18 09:38:03 -0500

    hw

--- todolist/src/components/Task.js
+++ todolist/src/components/Task.js
@@ -8,7 +8,7 @@ const Task = ({task, onDelete}) => {
     return(
         <div className="task">
             <h3> {task.text} <FaTimes style={{color:
-                'red', cursor: 'pointer'}}
+                'blue', cursor: 'pointer'}}
                 onClick={() => onDelete(task.id)}
                 /> 
                 </h3>
